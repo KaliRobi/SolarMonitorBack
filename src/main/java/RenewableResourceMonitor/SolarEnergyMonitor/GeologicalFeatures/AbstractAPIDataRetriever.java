6604a47f9896725c14f7ebd89d0fec1a2caf8b57
robi <kali_robi@hotmail.com>
2024-02-13 09:53:10 +0200

    AbstractAPIDataRetriever done

--- src/main/java/RenewableResourceMonitor/SolarEnergyMonitor/GeologicalFeatures/AbstractAPIDataRetriever.java
+++ src/main/java/RenewableResourceMonitor/SolarEnergyMonitor/GeologicalFeatures/AbstractAPIDataRetriever.java
@@ -1,13 +1,55 @@
 package RenewableResourceMonitor.SolarEnergyMonitor.GeologicalFeatures;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
+import java.net.http.HttpResponse.BodyHandlers;
 abstract class AbstractAPIDataRetriever {
 
-public void retrieveData(){
+public void getData(String urlString){
+
+    try {
+        URI targetUri = new URI(urlString);
+        HttpClient httpClient  = HttpClient.newHttpClient();
+        HttpRequest newRequest = HttpRequest.newBuilder(targetUri).build();
+        HttpResponse<String> response = httpClient.sendAsync(newRequest, BodyHandlers.ofString()).get();
+
+        System.out.println(response.body());
+
+    } catch (URISyntaxException | InterruptedException | ExecutionException e) {
+        throw new RuntimeException(e);
+    }
+
+}
+
+    public void postData(String urlString, String payload){
+
+        try {
+            URI targetUri = new URI(urlString);
+            HttpClient httpClient  = HttpClient.newHttpClient();
+            HttpRequest newRequest = HttpRequest.newBuilder()
+                    .uri(targetUri)
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(payload))
+                    .build();
+
+            HttpResponse<String> response = httpClient.send(newRequest, BodyHandlers.ofString());
+
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 
 
-//    https://overpass-api.de/api/interpreter?data=[out:json];area[name=%22L%C3%A4%C3%A4ne%20maakond%22]-%3E.searchArea;node(area.searchArea)[place];out;
 }
 
 
-}
+
--- src/main/java/RenewableResourceMonitor/SolarEnergyMonitor/GeologicalFeatures/OverpassApi.java
+++ src/main/java/RenewableResourceMonitor/SolarEnergyMonitor/GeologicalFeatures/OverpassApi.java
@@ -1,4 +1,12 @@
 package RenewableResourceMonitor.SolarEnergyMonitor.GeologicalFeatures;
 
-public class OverpassApi {
+public class OverpassApi extends AbstractAPIDataRetriever{
+
+      @Override
+    public void postData(String urlString, String payload) {
+        super.postData(urlString, payload);
+    }
+
+
 }
+//    https://overpass-api.de/api/interpreter?data=[out:json];area[name=%22L%C3%A4%C3%A4ne%20maakond%22]-%3E.searchArea;node(area.searchArea)[place];out;
